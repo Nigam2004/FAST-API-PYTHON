cdb201007fbbd6c9d36519a2a6f499d9f5da9907
Nigam Bisoyi <nigambisoyi2004@gmail.com>
2026-07-15 22:01:26 +0530

    send_queries_url

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
+# URL:-http://127.0.0.1:8000/
 def read_root():
     return {"message": "1st api through puthon"}
 
 @app.get("/home")
+# URL:-http://127.0.0.1:8000/home
 def  home_page():
     return ("this is my home page")
+
+@app.get("/customer")
+#input:-http://127.0.0.1:8000/customer?customer_id=123
+def customer(customer_id=int):
+    return{
+        "customer_id":customer_id,
+         "Cust_name":"Nigam",
+         "Cust_mob":"12346789"
+
+    }
